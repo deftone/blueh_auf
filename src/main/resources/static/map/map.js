2631adea79754c2016af680dc4fd2bfc4a704a4a
deftone <katrin.rose@poste.de>
2021-04-26 22:11:10 +0200

    2 seiten machen, eine fuer alle sichtbar und eine interaktive zum eingeben (todo)

--- src/main/resources/static/map/map.js
+++ src/main/resources/static/map/map.js
@@ -1,7 +1,7 @@
-      /* OSM & OL example code provided by https://mediarealm.com.au/ */
-      /*    https://mediarealm.com.au/articles/openstreetmap-openlayers-map-markers/ */
-      /*    https://openlayers.org/download/ */
-//      https://openlayers.org/en/latest/examples/icon.html
+/* OSM & OL example code provided by https://mediarealm.com.au/ */
+/*    https://mediarealm.com.au/articles/openstreetmap-openlayers-map-markers/ */
+/*    https://openlayers.org/download/ */
+/*    https://openlayers.org/en/latest/examples/icon.html */
 
 var map;
 //center for map, center of rossdorf
@@ -9,12 +9,8 @@ var mapLat = 49.86034;
 var mapLng = 8.77216;
 var mapDefaultZoom = 14.6;
 var list;
-var showNewLocation;
-var address;
-var coordinatesString;
 
-//die parameter muessen scheinbar gar nicht uebergeben werden??
-function initialize_map(list, showNewLocation, coordinatesString, address, errorMsg) {
+function initialize_map(list) {
   map = new ol.Map({
     target: "map",
     layers: [
@@ -30,45 +26,14 @@ function initialize_map(list, showNewLocation, coordinatesString, address, error
     })
   });
 
-  var messageDiv = document.getElementById("msg");
-  if (errorMsg!=null){
-    console.log(errorMsg);
-    //falls koordinaten falsch sind: stehen lassen und fehlermeldung anzeigen
-
-    messageDiv.innerHTML =errorMsg;
-  } else if (showNewLocation!=null) {
-    var saveButton = document.getElementById("saveButton");
-    saveButton.style.display="inline";
-    messageDiv.innerHTML ="Soll der Punkt gespeichert werden?";
-  }
-
-  addPoints(list, showNewLocation);
+  addPoints(list);
 }
 
-function addPoints(list, showNewLocation){
+function addPoints(list){
     console.log(list.length);
-    console.log(showNewLocation);
 
     for(var i in list) {
         add_map_point(list[i].latitude, list[i].longitude);
-    }
-
-    if (showNewLocation != null){
-      // neuer punkt soll in anderer farbe hinzugefuegt werden
-      add_map_point_blue(showNewLocation.latitude, showNewLocation.longitude);
-      //und koordinaten wieder in die boxen schreiben
-      document.getElementById("lat").value = showNewLocation.latitude;
-      document.getElementById("lon").value = showNewLocation.longitude;
-    }
-
-    // auch die google maps copy & paste coordinaten wieder reinschreiben, falls so eingetragen
-    if (coordinatesString != null){
-      document.getElementById("coordinatesInput").value = coordinatesString;
-    }
-
-    // und auch die adresse (falls sie eingegeben wurde)
-    if (address != null){
-      document.getElementById("addressInput").value = address;
     }
 }
 
@@ -85,25 +50,6 @@ function add_map_point(lat, lng) {
         anchorXUnits: "fraction",
         anchorYUnits: "fraction",
         src: "map/roterKreis.png"
-      })
-    })
-  });
-  map.addLayer(vectorLayer);
-}
-
-function add_map_point_blue(lat, lng) {
-  var vectorLayer = new ol.layer.Vector({
-    source:new ol.source.Vector({
-      features: [new ol.Feature({
-            geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857')),
-        })]
-    }),
-    style: new ol.style.Style({
-      image: new ol.style.Icon({
-        anchor: [0.5, 0.5],
-        anchorXUnits: "fraction",
-        anchorYUnits: "fraction",
-        src: "map/blauerKreis.png"
       })
     })
   });
